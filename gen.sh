37b7b7e210c875cc189fcc37648478bd0a23a634
Robert Pofuk <rpofuk@gmail.com>
2020-11-30 15:47:47 +0100

    Revert plantuml

--- gen.sh
+++ gen.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 plant_uml=$(mktemp)
 curl $plant_uml -k -s -L https://github.com/alpha-prosoft/alpha-readme-gen/raw/master/plantuml.jar
-python3 <(curl -s -L https://raw.githubusercontent.com/alpha-prosoft/alpha-readme-gen/main/plant-uml-render.py) $plant_uml
+python3 <(curl -s -L https://raw.githubusercontent.com/alpha-prosoft/alpha-readme-gen/master/plant-uml-render.py) $plant_uml
